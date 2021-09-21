f5e857a2032510fdaedb6bda03923c1fdcfbde00
ammuanju97 <anjutennyson@gmail.com>
2021-09-21 13:18:01 +0530

    updated

--- contactlist/settings.py
+++ contactlist/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +147,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+
+django_heroku.settings(locals())
